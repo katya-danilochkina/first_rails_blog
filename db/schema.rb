57821476dfe3b29d5149939696938c044cef41b3
katya-danilochkina <katya30108611@gmail.com>
2016-10-07 21:34:50 +0300

    Create multi-model form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160920133042) do
+ActiveRecord::Schema.define(version: 20161007164938) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "commenter"
@@ -27,6 +27,14 @@ ActiveRecord::Schema.define(version: 20160920133042) do
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_tags_on_post_id"
   end
 
 end
